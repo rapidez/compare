3ba813b270f4d71f12a484a148e0cf69312f2623
Roy Duineveld <royduineveld@gmail.com>
2021-01-27 13:38:35 +0100

    Title margin

--- src/views/overview.blade.php
+++ src/views/overview.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Compare')
 
 @section('content')
-    <h1 class="font-bold text-4xl">Compare</h1>
+    <h1 class="font-bold text-4xl mb-5">Compare</h1>
 
     <product-compare-overview />
 @endsection
