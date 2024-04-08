07f3ed76e7bf6e55b1541e2525f80ad2801436d2
Bob <bob@justbetter.nl>
2024-04-08 14:54:47 +0200

    Removed old stuff

--- resources/views/compare/overview.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('rapidez::layouts.app')
-
-@section('title', 'Compare')
-
-@section('content')
-    <h1 class="font-bold text-4xl mb-5">Compare</h1>
-
-    <product-compare-overview />
-@endsection
