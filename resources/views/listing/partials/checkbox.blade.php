b0949896a9a7aeb857679eaede1e0f6039d8113a
Jimmy <101801301+JimmyHoenderdaal@users.noreply.github.com>
2024-04-16 13:35:43 +0200

    Remove unnecessary div (#8)

--- resources/views/listing/partials/checkbox.blade.php
+++ resources/views/listing/partials/checkbox.blade.php
@@ -1,7 +1,5 @@
 <add-to-compare v-slot="addToCompare" :id="item.entity_id">
-    <div class="mt-1 md:mt-2">
-        <x-input.checkbox v-bind:checked="addToCompare.inCompare" @change="addToCompare.toggleCompare()">
-            <span class="text-xs font-medium text-inactive md:text-sm">@lang('Compare this product')</span>
-        </x-input.checkbox>
-    </div>
+    <x-input.checkbox v-bind:checked="addToCompare.inCompare" @change="addToCompare.toggleCompare()">
+        <span class="text-xs font-medium text-inactive md:text-sm">@lang('Compare this product')</span>
+    </x-input.checkbox>
 </add-to-compare>
