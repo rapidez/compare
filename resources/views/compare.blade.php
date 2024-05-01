8de288438be9e1fcb72cba1581fe8f5bbd70b8b3
Jordy de Vulder <jordy@justbetter.nl>
2024-05-01 13:30:31 +0200

    Comparison table styling (#9)

--- resources/views/compare.blade.php
+++ resources/views/compare.blade.php
@@ -5,33 +5,36 @@
 
 @section('content')
     <div class="container">
-        <h1 class="mb-5 text-3xl font-bold">{{ __('Compare') }}</h1>
+        <h1 class="mb-5 text-3xl font-bold">@lang('Compare')</h1>
         <product-compare v-slot="compare">
-            <div class="w-full overflow-x-scroll" v-cloak>
-                <div class="flex py-4 z-20 border-b space-x-16">
-                    <div class="sticky left-0 w-1/5 flex-shrink-0 bg-white"></div>
-                    <a class="relative w-1/5 flex-shrink-0" v-for="item, key in compare.items" :href="item.product.url_key">
-                        <x-heroicon-c-x-mark class="absolute top-1 right-1 w-6 h-6" @click.prevent="compare.remove(item.uid)" />
-                        <img :src="'/storage/{{ config('rapidez.store') }}/resizes/200/magento/catalog/product/' + compare.formatImage(item.product.image.url) + '.webp'" :alt="item.product.image.name">
-                        <strong class="mt-5">
-                            @{{ item.product.name }}
-                        </strong>
-                        <div class="text-14 flex items-center gap-2">
-                            <div v-if="item.product.price_range.minimum_price.final_price.value">
-                                <span class="relative">
-                                    @{{ item.product.price_range.minimum_price.final_price.value | priceDash }}
-                                </span>
+            <div class="relative text-sm">
+                <div class="absolute left-0 top-0 bottom-5 w-1/2 sm:w-1/3 lg:w-1/4 xl:w-1/5 bg-white z-10 max-sm:hidden"></div>
+                <div class="overflow-hidden">
+                    <div class="overflow-x-auto overflow-y-hidden pb-5 -mt-px max-sm:-ml-3 -mr-px">
+                        <div class="flex flex-col">
+                            <div class="flex *:px-3.5 *:pb-3 *:w-1/2 *:sm:w-1/3 *:lg:w-1/4 *:xl:w-1/5 *:shrink-0">
+                                <div class="max-sm:hidden"></div>
+                                <div class="relative flex flex-col" v-for="item, key in compare.items">
+                                    <button class="absolute top-1 right-1 cursor-pointer p-2.5 hover:bg-inactive-100 transition rounded-full group" @click.prevent="compare.remove(item.uid)">
+                                        <x-heroicon-o-x-mark stroke-width="3" class="size-4 text-inactive group-hover:text-neutral transition" />
+                                    </button>
+                                    <img class="object-contain h-28 sm:h-40" :src="item.product.image.url" :alt="item.product.image.name">
+                                    <a class="mt-1 font-semibold hover:underline" :href="item.product.url_key">
+                                        @{{ item.product.name }}
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="flex *:py-2.5 *:px-3.5 *:w-1/2 *:sm:w-1/3 *:lg:w-1/4 *:xl:w-1/5 *:shrink-0 *:border-t border-t *:-mt-px" :class="{ '*:!border-t-0 !border-t-0 ': key === 0 }" v-for="attribute, key in compare.attributes">
+                                <strong class="absolute left-0 !pl-0 z-10 truncate block max-sm:pt-3.5 max-sm:w-auto max-sm:right-0 pointer-events-none font-semibold">
+                                    @{{ attribute.label }}
+                                </strong>
+                                <div class="max-sm:hidden"></div>
+                                <div v-for="product, key in compare.items" class="flex !px-0 !py-0">
+                                    <div class="sticky w-px top-0 border-white left-1/2 sm:left-1/3 lg:left-1/4 xl:left-[20%] bg-inactive-110 shrink-0 max-sm:hidden"></div>
+                                    <div class="pb-1.5 px-3 sm:py-2.5 sm:px-3.5 max-sm:mt-9 max-sm:border-l">@{{ product.attributes.find((attr) => attr.code === attribute.code)?.value }}</div>
+                                </div>
                             </div>
                         </div>
-                    </a>
-                </div>
-
-                <div class="flex px-8 py-4 space-x-16 border-b" v-for="attribute, key in compare.attributes">
-                    <div class="sticky bg-white left-0 w-1/5 flex-shrink-0">
-                        <strong>@{{ attribute.label }}</strong>
-                    </div>
-                    <div class="w-1/5 flex-shrink-0" v-for="product, key in compare.items">
-                        @{{ product.attributes.find((attr) => attr.code === attribute.code)?.value }}
                     </div>
                 </div>
             </div>
