4218f2c65b5296851eb533e2c560935616545cc1
jordy <jordy@justbetter.nl>
2024-05-14 16:53:16 +0200

    restructured files and finetuning frontend

---
+++ resources/views/partials/no-results.blade.php
@@ -0,0 +1,6 @@
+<div class="flex flex-col gap-y-4 items-start">
+    <p>@lang('No products have been added to compare. Add products so you can compare them with each other.')</p>
+    <x-rapidez::button.outline href="/">
+        @lang('Back to home')
+    </x-rapidez::button.outline>
+</div>
