bd80f8355a994e24dbecad863fda2056448305df
indykoning <15870933+indykoning@users.noreply.github.com>
2026-01-20 14:54:13 +0100

    Make compare count an actual circle, use core checkox component

--- resources/views/listing/partials/checkbox.blade.php
+++ resources/views/listing/partials/checkbox.blade.php
@@ -1,5 +1,5 @@
 <add-to-compare v-slot="addToCompare" :id="item.entity_id">
-    <x-input.checkbox v-bind:checked="addToCompare.inCompare" v-on:change="addToCompare.toggleCompare()">
+    <x-rapidez::input.checkbox v-bind:checked="addToCompare.inCompare" v-on:change="addToCompare.toggleCompare()">
         <span class="text-xs font-medium text-muted md:text-sm">@lang('Compare this product')</span>
-    </x-input.checkbox>
+    </x-rapidez::input.checkbox>
 </add-to-compare>
