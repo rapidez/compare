217542a6923a5fa8e6d17c838a7ce3b45240ccf5
indykoning <15870933+indykoning@users.noreply.github.com>
2026-01-23 09:25:05 +0100

    Use core checkout component + circle style fix (#19)

--- resources/views/partials/button.blade.php
+++ resources/views/partials/button.blade.php
@@ -2,7 +2,9 @@
     <div class="fixed bottom-10 right-10 z-20" v-if="compare.items">
         <x-rapidez::button.outline class="relative bg-white" :href="route('rapidez-compare::compare')">
             {{ __('Compare') }}
-            <div class="text-white bg-primary rounded-full px-2 absolute -right-2 -top-2" v-txt="Math.round(compare.count)"></div>
+            <div class="text-white bg-primary rounded-full aspect-square px-2 absolute right-0 top-0 flex translate-x-1/2 -translate-y-1/2">
+                <span v-txt="Math.round(compare.count)" class="self-center">0</span>
+            </div>
         </x-rapidez::button.outline>
     </div>
 </product-compare>
